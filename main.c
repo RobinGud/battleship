69c46f65ea7eeab6c13e07e6c34668f895a6d2e8
Nikita Matsnev <niki-sim@mail.ru>
2017-12-07 01:20:02 +0300

    Added check for employment cell

--- main.c
+++ main.c
@@ -11,27 +11,53 @@ void FillField(char Field[SIZE][SIZE]) {
   }
 }
 
-void SetHorizontalShip(char Field[SIZE][SIZE], char X, int Y) {
- printf("Hor\n" );
+int SetHorizontalShip(char Field[SIZE][SIZE], int X, int Y, int LenShip) {
+  for(int i = X; i < X + LenShip; i++)
+    Field[Y][i] = '1';
 }
 
-void SetVerticalShip(char Field[SIZE][SIZE], char X, int Y) {
-  printf("two\n" );
+int SetVerticalShip(char Field[SIZE][SIZE], int X, int Y,int LenShip) {
+  for(int i = Y; i < Y + LenShip; i++)
+    Field[i][X] = '1';
+}
+
+int CheckHorizontalCoordinate(char Field[SIZE][SIZE], int X, int Y, int LenShip) {
+  if (X + LenShip <= SIZE) {
+    for(int i = X; i < X + LenShip; i++)
+      if (Field[Y][i] != '0')
+        return 1;
+  }
+  else
+    return 1;
+  SetHorizontalShip(Field, X, Y, LenShip);
+  return 0;
+}
+
+int CheckVerticalCoordinate(char Field[SIZE][SIZE], int X, int Y, int LenShip) {
+  if (Y + LenShip <= SIZE) {
+    for(int i = Y; i < Y + LenShip; i++)
+      if (Field[i][X] != '0')
+        return 1;
+  }
+  else
+    return 1;
+  SetVerticalShip(Field, X, Y, LenShip);
+  return 0;
 }
 
 void InputShip(int LenShip, char Field[SIZE][SIZE] ) {
-  int Y = -1;
-  char X, Direction;
-  while (1 == 1) {
+  int Y, Status = 1, X;
+  char charX, Direction;
+  while (Status == 1) {
     printf("Input left top coordinate and direction for %d deck ship(x, y, [h, v]): ", LenShip);
-    scanf("%1c%1d%1c", &X, &Y, &Direction);
+    scanf("%1c%1d%1c", &charX, &Y, &Direction);
     fflush(stdin);
-    if ((X >= 'a') && (X <= 'j') && (Y >= 0) && (Y <= 9) && ((Direction == 'h') || (Direction == 'v')))
-      break;
-    else
+    X = (int)charX - 97;
+    if ((X >= 0) && (X <= 9) && (Y >= 0) && (Y <= 9) && ((Direction == 'h') || (Direction == 'v')))
+      (Direction == 'h') ? (Status = CheckHorizontalCoordinate(Field, X, Y, LenShip)) : (Status = CheckVerticalCoordinate(Field, X, Y, LenShip));
+    if (Status == 1)
       printf("Error. Try again.\n");
   }
-    (Direction == 'h') ? SetHorizontalShip(Field, X, Y) : SetVerticalShip(Field, X, Y);
 }
 
 void OutPutField(char Field[SIZE][SIZE]) {
@@ -59,7 +85,8 @@ int main () {
   OutPutField(PlayerField);
 
   for (int i = 0; i < SIZE; i++) {
-  InputShip(LenShips[i], PlayerField);
+    InputShip(LenShips[i], PlayerField);
+    OutPutField(PlayerField);
   }
 
   system ("pause");
